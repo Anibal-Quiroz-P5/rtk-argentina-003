73a269acad60e9654780ca6eeaf6737ec9fb6cb2
Anibal-Quiroz-P5 <anibal.quiroz.p5@gmail.com>
2024-07-28 19:02:19 -0300

    fix card CorreoContact

--- src/app/components/CorreoContact.tsx
+++ src/app/components/CorreoContact.tsx
@@ -231,6 +231,203 @@
 // export default CorreoContact;
 
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// "use client";
+
+// import Link from "next/link";
+// import React, { useState } from "react";
+// import AlertaPersonalizada from "./AlertaPersonalizada"; // Asegúrate de que la ruta sea correcta
+
+// const CorreoContact = () => {
+//   const styles = {
+//     clipPath:
+//       "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+//   };
+
+//   const [formData, setFormData] = useState({
+//     firstName: "",
+//     lastName: "",
+//     email: "",
+//     message: "",
+//   });
+
+//   const [submitting, setSubmitting] = useState(false);
+//   const [alertVisible, setAlertVisible] = useState(false);
+//   const [alertMessage, setAlertMessage] = useState("");
+
+//   const handleInputChange = (e: { target: { name: any; value: any } }) => {
+//     const { name, value } = e.target;
+
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     });
+//   };
+
+//   const handleSubmit = async (e: { preventDefault: () => void }) => {
+//     e.preventDefault();
+//     setSubmitting(true);
+
+//     // send email
+//     const response = await fetch("api/contact-us", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//         Accept: "application/json",
+//       },
+//       body: JSON.stringify(formData),
+//     });
+
+//     const { success, error } = await response.json();
+
+//     if (success) {
+//       setAlertMessage("Su consulta ha sido enviada!");
+//       setAlertVisible(true);
+//     } else if (error) {
+//       console.error(error);
+//       setAlertMessage("Error al enviar su consulta: " + error);
+//       setAlertVisible(true);
+//     }
+
+//     setSubmitting(false);
+//   };
+
+//   return (
+//     <div className=" pt-[55px] lg:pt-24 pb-[55px]  2xl:pt-[46px] 2xl:pb-[40px]">
+//       <AlertaPersonalizada
+//         mensaje={alertMessage}
+//         visible={alertVisible}
+//         onClose={() => setAlertVisible(false)}
+//       />
+//       <div id="contacto" className="bg-[purple-100] 2xl:pt-14 2xl:pb-14">
+//         <div className="mx-auto  px-6   lg:px-8 bg-[#E3F6E7] 2xl:w-[898px] h-[636px] 2xl:h-[682px] lg:w-[89%] rounded-3xl">
+          
+
+//           <div className=" text-center  ">
+//             <h2 className=" font-monserrat font-extrabold text-[#111827] text-[16px] pt-10 2xl:pt-[56px]">
+//               CONTACTO
+//             </h2>
+//             <p className=" lg:text-[18px]  text-[#111827]  font-inter font-regular text-[16px] 2xl:text-[16px]   2xl:pt-[10px]">
+//               Escribinos tu mensaje, te contestaremos por mail a la brevedad.
+//             </p>
+//           </div>
+//           <form
+//             action="#"
+//             method="POST"
+//             className="mx-auto  max-w-xl  2xl:pt-[15px]"
+//             onSubmit={handleSubmit}
+//           >
+//             <div className="grid grid-cols-1 gap-y-4   ">
+
+
+
+
+//               <div  className="sm:col-span-2">
+//                 <label
+//                   htmlFor="firstName"
+//                   className="block text-sm leading-6 text-gray-900   font-inter font-semibold  text-[16px] 2xl:text-[16px] "
+//                 >
+//                   Nombre
+//                 </label>
+//                 <div className="mt-2.5">
+//                   <input
+//                     type="text"
+//                     name="firstName"
+//                     id="firstName"
+//                     autoComplete="given-name"
+//                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+//                     onChange={handleInputChange}
+//                   />
+//                 </div>
+//               </div>
+
+
+//               <div className="sm:col-span-2">
+//                 <label
+//                   htmlFor="email"
+//                   className="block text-sm  leading-6 text-gray-900   font-inter font-semibold text-[16px] 2xl:text-[16px]"
+//                 >
+//                   Email
+//                 </label>
+//                 <div className="mt-2.5">
+//                   <input
+//                     type="email"
+//                     name="email"
+//                     id="email"
+//                     autoComplete="email"
+//                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+//                     onChange={handleInputChange}
+//                   />
+//                 </div>
+//               </div>
+
+
+
+//               <div className="sm:col-span-2">
+//                 <label htmlFor="service" className="block text-sm font-semibold leading-6 text-gray-900">Area de aplicación de RTK</label>
+//                 <select id="service" name="service" className="mt-2.5 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={handleInputChange}>
+//                   <option value="">Selecciona un servicio</option>
+//                   <option value="agrimensura">AGRIMENSURA</option>
+//                   <option value="drones">DRONES</option>
+//                   <option value="agricultura">AGRICULTURA</option>
+//                   <option value="otros">OTROS</option>
+//                 </select>
+//               </div>
+
+
+
+
+//               <div className="sm:col-span-2">
+//                 <label
+//                   htmlFor="message"
+//                   className="block text-sm leading-6 text-gray-900    font-inter font-semibold text-[16px] 2xl:text-[16px]"
+//                 >
+//                   Mensaje
+//                 </label>
+//                 <div className="mt-2.5">
+//                   <textarea
+//                     name="message"
+//                     id="message"
+//                     rows={4}
+//                     className="block w-full rounded-md border-0 px-3.5  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+//                     onChange={handleInputChange}
+//                   ></textarea>
+//                 </div>
+//               </div>
+
+
+
+
+//               <div className="flex gap-x-4 sm:col-span-2"></div>
+//             </div>
+//             <div className="flex justify-center mt-6 2xl:mt-10">
+//               <button
+//                 type="submit"
+//                 className="block lg:w-[176px] lg:h-[50px] rounded-md bg-[#F95901] px-3.5 py-2.5 text-center text-[16px] font-semibold text-white shadow-sm hover:bg-[#f3915d] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+//               >
+//                 Enviar
+//               </button>
+//             </div>
+//           </form>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CorreoContact;
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 "use client";
 
 import Link from "next/link";
@@ -238,15 +435,10 @@ import React, { useState } from "react";
 import AlertaPersonalizada from "./AlertaPersonalizada"; // Asegúrate de que la ruta sea correcta
 
 const CorreoContact = () => {
-  const styles = {
-    clipPath:
-      "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-  };
-
   const [formData, setFormData] = useState({
     firstName: "",
-    lastName: "",
     email: "",
+    service: "",
     message: "",
   });
 
@@ -254,7 +446,7 @@ const CorreoContact = () => {
   const [alertVisible, setAlertVisible] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
 
-  const handleInputChange = (e: { target: { name: any; value: any } }) => {
+  const handleInputChange = (e: { target: { name: string; value: string } }) => {
     const { name, value } = e.target;
 
     setFormData({
@@ -265,6 +457,14 @@ const CorreoContact = () => {
 
   const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
+
+    // Validar si los campos están vacíos
+    if (!formData.firstName || !formData.email || !formData.service || !formData.message) {
+      setAlertMessage("Por favor, completa todos los campos.");
+      setAlertVisible(true);
+      return;
+    }
+
     setSubmitting(true);
 
     // send email
@@ -292,35 +492,33 @@ const CorreoContact = () => {
   };
 
   return (
-    <div className=" pt-[55px] lg:pt-24 pb-[55px]  2xl:pt-[46px] 2xl:pb-[40px]">
+    <div className="pt-[55px] lg:pt-24 pb-[55px] 2xl:pt-[46px] 2xl:pb-[40px]">
       <AlertaPersonalizada
         mensaje={alertMessage}
         visible={alertVisible}
         onClose={() => setAlertVisible(false)}
       />
       <div id="contacto" className="bg-[purple-100] 2xl:pt-14 2xl:pb-14">
-        <div className="mx-auto  px-6   lg:px-8 bg-[#E3F6E7] 2xl:w-[898px] h-[636px] 2xl:h-[682px] lg:w-[89%] rounded-3xl">
-          
-
-          <div className=" text-center  ">
-            <h2 className=" font-monserrat font-extrabold text-[#111827] text-[16px] pt-10 2xl:pt-[56px] 2xl:text-[43px]">
+        <div className="mx-auto px-6 lg:px-8 bg-[#E3F6E7] 2xl:w-[898px] h-[636px] 2xl:h-[682px] lg:w-[89%] rounded-3xl">
+          <div className="text-center">
+            <h2 className="font-monserrat font-extrabold text-[#111827] text-[16px] pt-10 2xl:pt-[56px]">
               CONTACTO
             </h2>
-            <p className=" lg:text-[18px]  text-[#111827]  font-inter font-regular text-[16px] 2xl:text-[16px]   2xl:pt-[10px]">
+            <p className="lg:text-[18px] text-[#111827] font-inter font-regular text-[16px] 2xl:text-[16px] 2xl:pt-[10px]">
               Escribinos tu mensaje, te contestaremos por mail a la brevedad.
             </p>
           </div>
           <form
             action="#"
             method="POST"
-            className="mx-auto  max-w-xl  2xl:pt-[15px]"
+            className="mx-auto max-w-xl 2xl:pt-[15px]"
             onSubmit={handleSubmit}
           >
-            <div className="grid grid-cols-1 gap-y-4   ">
-              <div>
+            <div className="grid grid-cols-1 gap-y-4">
+              <div className="sm:col-span-2">
                 <label
                   htmlFor="firstName"
-                  className="block text-sm leading-6 text-gray-900   font-inter font-semibold  text-[16px] 2xl:text-[16px] "
+                  className="block text-sm leading-6 text-gray-900 font-inter font-semibold text-[16px] 2xl:text-[16px]"
                 >
                   Nombre
                 </label>
@@ -330,33 +528,17 @@ const CorreoContact = () => {
                     name="firstName"
                     id="firstName"
                     autoComplete="given-name"
+                    placeholder="Juan"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     onChange={handleInputChange}
                   />
                 </div>
               </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="company"
-                  className="block text-sm  leading-6 text-gray-900    font-inter font-semibold text-[16px] 2xl:text-[16px]"
-                >
-                  Area de aplicación de RTK
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="text"
-                    name="company"
-                    id="company"
-                    autoComplete="organization"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    onChange={handleInputChange}
-                  />
-                </div>
-              </div>
+
               <div className="sm:col-span-2">
                 <label
                   htmlFor="email"
-                  className="block text-sm  leading-6 text-gray-900   font-inter font-semibold text-[16px] 2xl:text-[16px]"
+                  className="block text-sm leading-6 text-gray-900 font-inter font-semibold text-[16px] 2xl:text-[16px]"
                 >
                   Email
                 </label>
@@ -366,15 +548,38 @@ const CorreoContact = () => {
                     name="email"
                     id="email"
                     autoComplete="email"
+                    placeholder="juan@ejemplo.com"
                     className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     onChange={handleInputChange}
                   />
                 </div>
               </div>
+
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="service"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Área de aplicación de RTK
+                </label>
+                <select
+                  id="service"
+                  name="service"
+                  className="mt-2.5 block w-full rounded-md border-0 bg-white py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={handleInputChange}
+                >
+                  <option value="">Selecciona un servicio</option>
+                  <option value="agrimensura">AGRIMENSURA</option>
+                  <option value="drones">DRONES</option>
+                  <option value="agricultura">AGRICULTURA</option>
+                  <option value="otros">OTROS</option>
+                </select>
+              </div>
+
               <div className="sm:col-span-2">
                 <label
                   htmlFor="message"
-                  className="block text-sm leading-6 text-gray-900    font-inter font-semibold text-[16px] 2xl:text-[16px]"
+                  className="block text-sm leading-6 text-gray-900 font-inter font-semibold text-[16px] 2xl:text-[16px]"
                 >
                   Mensaje
                 </label>
@@ -383,11 +588,13 @@ const CorreoContact = () => {
                     name="message"
                     id="message"
                     rows={4}
-                    className="block w-full rounded-md border-0 px-3.5  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                    placeholder="Escribe tu mensaje aquí..."
+                    className="block w-full rounded-md border-0 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                     onChange={handleInputChange}
                   ></textarea>
                 </div>
               </div>
+
               <div className="flex gap-x-4 sm:col-span-2"></div>
             </div>
             <div className="flex justify-center mt-6 2xl:mt-10">
