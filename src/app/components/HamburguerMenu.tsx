38126f9239b2fc6a56c10cedb957e17260051915
Anibal-Quiroz-P5 <anibal.quiroz.p5@gmail.com>
2024-08-08 16:51:51 -0300

    fix size hamburguer menu

--- src/app/components/HamburguerMenu.tsx
+++ src/app/components/HamburguerMenu.tsx
@@ -236,7 +236,7 @@ const HamburguerMenu: React.FC<HamburguerMenuProps> = ({
         <ul className="text-center">
           <li>
             <div
-              className="text-[#002E1E] font-semibold cursor-pointer text-[22px] mb-4 hover:text-[#e6f74e] transition duration-300"
+              className="text-[#002E1E] font-semibold cursor-pointer text-[22px] mb-4 hover:text-[#F95901] transition duration-300"
               onClick={() => handleLinkClick("#servicio")}
             >
               Inicio
@@ -244,7 +244,7 @@ const HamburguerMenu: React.FC<HamburguerMenuProps> = ({
           </li>
           <li>
             <div
-              className="text-[#002E1E] font-semibold cursor-pointer text-[22px] mb-4 hover:text-[#ee4c46] transition duration-300"
+              className="text-[#002E1E] font-semibold cursor-pointer text-[22px] mb-4 hover:text-[#F95901] transition duration-300"
               onClick={() => handleLinkClick("#aplicaciones")}
             >
               Aplicaciones
@@ -252,7 +252,7 @@ const HamburguerMenu: React.FC<HamburguerMenuProps> = ({
           </li>
           <li>
             <div
-              className="text-[#002E1E] font-semibold cursor-pointer text-[22px] mb-4 hover:text-[#3cc7c7] transition duration-300"
+              className="text-[#002E1E] font-semibold cursor-pointer text-[22px] mb-4 hover:text-[#F95901] transition duration-300"
               onClick={() => handleLinkClick("#quienes-somos")}
             >
               Quienes somos
@@ -260,7 +260,7 @@ const HamburguerMenu: React.FC<HamburguerMenuProps> = ({
           </li>
           <li>
             <div
-              className="text-[#002E1E] font-semibold cursor-pointer text-[22px] mb-4 hover:text-[#36485C] transition duration-300"
+              className="text-[#002E1E] font-semibold cursor-pointer text-[22px] mb-4 hover:text-[#F95901] transition duration-300"
               onClick={() => handleLinkClick("#contacto")}
             >
               Contacto
