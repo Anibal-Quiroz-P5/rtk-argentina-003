de4916f44700a1a99356a497444591bf7dfd0c36
Anibal-Quiroz-P5 <anibal.quiroz.p5@gmail.com>
2024-08-19 19:33:35 -0300

    fix footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -58,10 +58,10 @@ import Link from "next/link";
 export const Footer = () => {
   return (
     <div className="relative left-1/2 -translate-x-1/2 w-screen ">
-    <div className=" pt-[0px] pb-[40px] lg:pt-12 2xl:pt-0 2xl:pb-[50px] bg-[#002E1E] ">
-      {/* <div className='flex items-center justify-center gap-x-[76px] pt-[40px] '> */}
+      <div className=" pt-[0px] pb-[40px] lg:pt-12 2xl:pt-0 2xl:pb-[50px] bg-[#002E1E] ">
+        {/* <div className='flex items-center justify-center gap-x-[76px] pt-[40px] '> */}
 
-      {/* <div className="flex flex-row  justify-center gap-x-40  ">
+        {/* <div className="flex flex-row  justify-center gap-x-40  ">
         <div>
           <div className="flex gap-4 items-center">
             <Image src={Whatsapp} alt="Facebook" width={42} height={42} />
@@ -84,27 +84,25 @@ export const Footer = () => {
         </div>
       </div> */}
 
-      {/* <div className="flex flex-col lg:flex lg:flex-row justify-center items-center sm:gap-x-10 2xl:gap-x-20 "> */}
-      <div className="flex flex-col lg:flex lg:flex-row justify-center  sm:gap-x-10 pt-6 2xl:gap-x-20 2xl:pt-12">
-        <div className="flex flex-row  items-center">
-          <div className="pl-6">
+        {/* <div className="flex flex-col lg:flex lg:flex-row justify-center items-center sm:gap-x-10 2xl:gap-x-20 "> */}
+        <div className="flex flex-col lg:flex lg:flex-row justify-center  sm:gap-x-10 pt-6 2xl:gap-x-20 2xl:pt-12">
+          <div className="flex flex-row  items-center">
+            <div className="pl-6">
+              {/* <Image src={Whatsapp} alt="Whatsapp" width={34} height={42} /> */}
+            </div>
+            <div className="pl-3 ">
+              <a
+                /* href="https://www.whatsapp.com" */
+                href="https://wa.me/5491124940670"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex gap-4 items-center p-0 pl-0 hover:bg-green-700 rounded text-white"
+              >
+                <Image src={Whatsapp} alt="Whatsapp" width={34} height={42} />
+                <span className="text-[16px] pl-[2px]">+54911-24940670</span>
+              </a>
 
-          <Image src={Whatsapp} alt="Whatsapp" width={34} height={42} />
-          </div>
-          <div className="pl-3 ">
-
-          <a
-            /* href="https://www.whatsapp.com" */
-            href="https://wa.me/5491124940670"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex gap-4 items-center p-0 pl-0 hover:bg-gray-200 rounded text-white"
-          >
-            {/* <Image src={Whatsapp} alt="Whatsapp" width={34} height={42} /> */}
-            <span className="text-[16px] pl-[2px]">+54911-24940670</span>
-          </a>
-
-          {/* <a
+              {/* <a
             
             href="https://wa.me/5491151555966"
             target="_blank"
@@ -114,10 +112,10 @@ export const Footer = () => {
             
             <span className="text-[16px]">+54911-51555966</span>
           </a> */}
+            </div>
           </div>
-        </div>
 
-        {/*         <a
+          {/*         <a
           href="https://www.facebook.com"
           target="_blank"
           rel="noopener noreferrer"
@@ -126,16 +124,17 @@ export const Footer = () => {
           <Image src={Facebook} alt="Facebook" width={34} height={42} />
           <span>@rtk-argentina---</span>
         </a> */}
-        <a
-          href="mailto:soporte@rtkarg.com"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex gap-4 items-center p-2   hover:bg-gray-200 rounded pl-6"
-        >
-          <Image src={X2} alt="Twitter" width={34} height={42} />
-          <span className="text-[16px] text-white">soporte@rtkarg.com</span>
-        </a>
-        {/*         <a
+          <a
+            href="mailto:soporte@rtkarg.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            /* className="flex gap-4 items-center p-2   hover:bg-gray-200 rounded pl-6" */
+            className="flex gap-4 items-center p-2   hover:bg-green-700 rounded pl-6"
+          >
+            <Image src={X2} alt="Twitter" width={34} height={42} />
+            <span className="text-[16px] text-white">soporte@rtkarg.com</span>
+          </a>
+          {/*         <a
           href="https://www.instagram.com"
           target="_blank"
           rel="noopener noreferrer"
@@ -144,7 +143,7 @@ export const Footer = () => {
           <Image src={Instagram} alt="Instagram" width={32} height={42} />
           <span>@rtk-argentina---</span>
         </a> */}
-        {/* <a
+          {/* <a
           href="https://www.linkedin.com/in/anibal-quiroz-full-stack-javascript/"
           target="_blank"
           rel="noopener noreferrer"
@@ -153,9 +152,9 @@ export const Footer = () => {
           <Image src={Linkedin} alt="Linkedin" width={34} height={42} />
           <span className="text-[16px]">@rtk-argentina---</span>
         </a> */}
-      </div>
+        </div>
 
-      {/* <div className='flex flex-col items-center justify-center gap-y-[40px] pt-[40px]'>
+        {/* <div className='flex flex-col items-center justify-center gap-y-[40px] pt-[40px]'>
   <div className='flex items-center gap-x-4'>
     <Image src={Facebook} alt="Icon 1" />
     <span>Texto 1</span>
@@ -170,7 +169,7 @@ export const Footer = () => {
   </div>
 </div> */}
 
-      {/* ----------------------------------------------------------------------------------
+        {/* ----------------------------------------------------------------------------------
 
       <div className="flex items-center justify-center gap-x-[12px] pt-14">
         <Image src={Logo} alt="Logo" width={40} />
@@ -179,21 +178,19 @@ export const Footer = () => {
 
     ---------------------------------------------------------------------------------- */}
 
-      <nav>
-        <Link
-          href="/"
-          className="font-semibold text-white hover:text-gray-300 "
-        >
-          <div className="flex items-center justify-center gap-x-[12px]  pt-8">
-            <Image src={Logo} alt="Logo" width={40} />
-            <p className="font-bold  text-[17px] text-white">
-              RTK-Argentina
-            </p>
-          </div>
-        </Link>
-      </nav>
+        <nav>
+          <Link
+            href="/"
+            className="font-semibold text-white hover:text-gray-300 "
+          >
+            <div className="flex items-center justify-center gap-x-[12px]  pt-8">
+              <Image src={Logo} alt="Logo" width={40} />
+              <p className="font-bold  text-[17px] text-white">RTK-Argentina</p>
+            </div>
+          </Link>
+        </nav>
 
-      {/* <ul className="flex flex-col items-center gap-y-[32px] pt-[56px] text-[#36485C] sm:flex-row sm:justify-center sm:gap-x-5 sm:pt-5">
+        {/* <ul className="flex flex-col items-center gap-y-[32px] pt-[56px] text-[#36485C] sm:flex-row sm:justify-center sm:gap-x-5 sm:pt-5">
         <Link href="#servicio">
           <li>Servicio</li>
         </Link>
@@ -208,10 +205,10 @@ export const Footer = () => {
         </Link>
       </ul> */}
 
-      <p className="pt-[56px] text-center text-[16px] font-medium text[#5F7896] sm:pt-5 text-white">
-        © Copyright 2024. RTK-Argentina. All rights reserved.
-      </p>
-    </div>
+        <p className="pt-[56px] text-center text-[16px] font-medium text[#5F7896] sm:pt-5 text-white">
+          © Copyright 2024. RTK-Argentina. All rights reserved.
+        </p>
+      </div>
     </div>
   );
 };
