3c25658248fa508381869b8519412af5c83ea533
Anibal-Quiroz-P5 <anibal.quiroz.p5@gmail.com>
2025-03-20 17:07:27 -0300

    Fix-Navbar

--- src/app/cursos-seminarios/page.tsx
+++ src/app/cursos-seminarios/page.tsx
@@ -10,7 +10,7 @@ const News = () => {
   return (
     <div className="lg:pt-[30px] pt-[50px]   bg-gradient-to-b from-custom-green-start to-custom-green-end items-start xl:items-start justify-center font-monserrat font-extrabold lg:text-[58px] text-[#FFFFFF]  h-[115px] lg:h-[290px]  2xl:h-[290px] ">
       <div id="cursos" className="flex flex-col items-center justify-center min-h-screen bg-[#002E1E] text-white p-10 ">
-            <h1 className=" font-bold mb-8 mt-4">CURSOS</h1>
+            <h1 className=" font-bold mb-8 mt-4 text-[18px]">CURSOS</h1>
               <div className="flex flex-col gap-8 px-7 ">
                 <div className="lg:pb-8 lg:pt-10" >
                   <Image
